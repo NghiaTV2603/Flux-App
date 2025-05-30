0d49b2e4eb7a3c20e12c175348930295dc166089
Marco <nghiatv@avada.email>
2025-05-30 10:43:00 +0700

    update env

--- services/auth-service/src/auth/dto/register.dto.ts
+++ services/auth-service/src/auth/dto/register.dto.ts
@@ -1,16 +1,25 @@
-import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsNotEmpty,
+} from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsString()
   @MinLength(3)
   @MaxLength(20)
+  @IsNotEmpty()
   username: string;
 
   @IsString()
   @MinLength(6)
   @MaxLength(50)
+  @IsNotEmpty()
   password: string;
 }
